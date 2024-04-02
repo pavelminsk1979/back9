00b213cc8d7c80bef8d30a7f18d8d7ba2582a77d
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-04-02 08:40:33 +0300

    fix..............

--- src/servisces/securityDevices-service.ts
+++ src/servisces/securityDevices-service.ts
@@ -55,11 +55,13 @@ export const securityDevicesService = {
             code: ResultCode.Incorrect
         }
 
-
-
         if (deviceId !== result.deviceId) return {
-            code: ResultCode.NotFound
+            code: ResultCode.Failure
         }
+
+     /*   if (deviceId !== result.deviceId) return {
+            code: ResultCode.NotFound
+        }*/
 
          await usersDevicesRepository.deleteDeviceById(deviceId)
 
